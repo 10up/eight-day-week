d2596f7697e8e9af1d33cf5e290ed11263886d03
Dhanendran Rajagopal <dhanendran.rajagopal@gmail.com>
2022-07-15 11:29:28 +0530

    print issue

---
+++ tests/cypress/e2e/print.test.js
@@ -0,0 +1,19 @@
+describe('Publish a new print issue', () => {
+	before(() => {
+		cy.login();
+	});
+
+	it("Open new print issue page", () => {
+		cy.visit(`/wp-admin/post-new.php?post_type=print-issue`);
+		cy.get("#title").type("Print Title 1");
+		cy.get("#pi-section-add").click();
+		cy.get("#pi-section-name").type("Section title 1");
+		cy.get("#pi-section-add-confirm").click();
+		cy.wait(1000)
+		cy.get(".pi-article-add:visible").click();
+		cy.get(".pi-article-title:visible").type("Hello");
+		cy.wait(500);
+		cy.get(".ui-menu-item-wrapper:visible").click();
+		cy.get("#normal-sortables .button-primary:visible").click();
+	});
+});
