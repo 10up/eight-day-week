c0892b7eb68be955b3b83964c96be025ad0a0745
Dhanendran Rajagopal <dhanendran.rajagopal@gmail.com>
2022-07-25 19:54:04 +0530

    removed wait

--- tests/cypress/e2e/print.test.js
+++ tests/cypress/e2e/print.test.js
@@ -9,10 +9,10 @@ describe('Publish a new print issue', () => {
 		cy.get("#pi-section-add").click();
 		cy.get("#pi-section-name").type("Section title 1");
 		cy.get("#pi-section-add-confirm").click();
-		cy.wait(1000)
-		cy.get(".pi-article-add:visible").click();
+		cy.get(".pi-article-add:visible").should('exist').click();
 		cy.get(".pi-article-title:visible").type("Hello");
-		cy.wait(1000);
+		cy.intercept('GET', '/wp-admin/admin-ajax.php*').as('ajaxRequest');
+		cy.wait('@ajaxRequest').its('response.statusCode').should('eq', 200);
 		cy.get("body").then($body => {
 			if ($body.find(".ui-menu-item").length > 0) {
 				cy.get(".ui-menu-item").click();
