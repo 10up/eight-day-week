852ffae6c92b99ad6fdcdcd0cbb5d62f5048f164
Sudip Dadhaniya <61381229+sudip-d@users.noreply.github.com>
2024-06-07 19:36:42 +0530

    Modified and added new test cases

---
+++ tests/cypress/e2e/issue-status.test.js
@@ -0,0 +1,26 @@
+describe('Issue Status', () => {
+	before(() => {
+		cy.login();
+	});
+
+    it("Create Issue Status", () => {
+        cy.visit(`wp-admin/edit-tags.php?taxonomy=print_issue_status&post_type=print-issue`);
+        cy.get('#tag-name').type('Active-01');
+        cy.get('#submit').click();
+      });
+
+      it("Edit Issue Status", () => {
+        cy.visit(`wp-admin/edit-tags.php?taxonomy=print_issue_status&post_type=print-issue`);
+        cy.get('[aria-label="“Active-01” (Edit)"]').click();
+        cy.get('#name').clear('Active-02');
+        cy.get('#name').type('Active-02')
+        cy.get('.button').click();
+      });
+
+      it("Delete Issue Status", () => {
+        cy.visit(`wp-admin/edit-tags.php?taxonomy=print_issue_status&post_type=print-issue`);
+        cy.get('[aria-label="“Active-02” (Edit)"]').click();
+        cy.get('.delete').click();
+      });
+
+    });
