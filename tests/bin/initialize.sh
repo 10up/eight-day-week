8c4e80c14c2f833a26add461e7ae9284410d15b0
Dharmesh Patel <dspatel44@gmail.com>
2024-09-06 17:50:45 +0530

    Add create user in initialize.sh for E2E tests.

--- tests/bin/initialize.sh
+++ tests/bin/initialize.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 npm run env run tests-wordpress "chmod -c ugo+w /var/www/html"
 npm run env run tests-cli "wp rewrite structure '/%postname%/' --hard"
+
+npm run env run tests-cli "wp user create editor editor@eightdayweek.com --user_pass=password --role=editor"
