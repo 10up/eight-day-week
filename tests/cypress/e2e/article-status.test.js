1d28601d5dabde9430daf1fc868d78424b46509f
Dharmesh Patel <dspatel44@gmail.com>
2025-03-31 10:59:34 +0530

    Fix Failing tests.

--- tests/cypress/e2e/article-status.test.js
+++ tests/cypress/e2e/article-status.test.js
@@ -10,31 +10,30 @@ describe('Article Status', () => {
         
         // Verify successful creation
         cy.get('.notice-success').should('be.visible')
-            .and('contain', 'Article Status added.');
+            .and('contain', 'Item added.');
         cy.get('#the-list').should('contain', 'Active-01');
     });
 
     it("Edit Article Status", () => {
         cy.visit(`wp-admin/edit-tags.php?taxonomy=pi-article-status&post_type=print-issue`);
-        cy.get('[aria-label="Active-01 (Edit)"]').click();
+        cy.get('a.row-title').contains("Active-01").first().click();
         cy.get('#name').clear();
         cy.get('#name').type('Active-02');
         cy.get('.button').click();
         
         // Verify successful edit
         cy.get('.notice-success').should('be.visible')
-            .and('contain', 'Article Status updated.');
+            .and('contain', 'Item updated.');
+        cy.visit(`wp-admin/edit-tags.php?taxonomy=pi-article-status&post_type=print-issue`);
         cy.get('#the-list').should('contain', 'Active-02');
     });
 
     it("Delete Article Status", () => {
         cy.visit(`wp-admin/edit-tags.php?taxonomy=pi-article-status&post_type=print-issue`);
-        cy.get('[aria-label="Active-02 (Edit)"]').click();
-        cy.get('.delete').click();
+        cy.get('a.row-title').contains("Active-02").first().click();
+        cy.get('a.delete').click();
         
         // Verify successful deletion
-        cy.get('.notice-success').should('be.visible')
-            .and('contain', 'Article Status deleted.');
         cy.get('#the-list').should('not.contain', 'Active-02');
     });
 });
