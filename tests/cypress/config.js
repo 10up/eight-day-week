535adef18fc4d3cf894060c647acb10965cfd431
Darin Kotter <darin.kotter@gmail.com>
2026-05-08 12:20:49 -0600

    Update outdated node dependencies

--- tests/cypress/config.js
+++ tests/cypress/config.js
@@ -30,6 +30,7 @@ module.exports = defineConfig({
     specPattern: 'tests/cypress/e2e/**/*.test.{js,jsx,ts,tsx}',
     supportFile: 'tests/cypress/support/e2e.js'
   },
+  allowCypressEnv: false
 });
 
 /**
